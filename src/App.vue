5232ca29528e8e97ce2cb804f9ddb8ef65a19fc6
monees007 <monees007@outlook.com>
2024-06-18 13:57:39 +0530

    Framework done

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <v-app>
     <v-navigation-drawer v-model="drawer">
-      <v-list  :items="level1"></v-list>
+      <v-list>
+        <v-list-item @click="tap1(item)" v-bind:key v-for="(item,key) in level1"> {{ item }}</v-list-item>
+      </v-list>
 
     </v-navigation-drawer>
     <v-navigation-drawer v-model="drawer">
-      <!--  -->
-
+      <v-list>
+        <v-list-item @click="tap2(item)" v-bind:key v-for="(item,key) in level2"> {{ item }}</v-list-item>
+      </v-list>
     </v-navigation-drawer>
     <v-navigation-drawer v-model="drawer">
-      <!--  -->
+      <v-list>
+        <v-list-item @click="tap3(item)" v-bind:key v-for="(item,key) in level3"> {{ item }}</v-list-item>
+      </v-list>      <!--  -->
 
     </v-navigation-drawer>
 
@@ -20,6 +25,7 @@
     </v-app-bar>
 
     <v-main>
+      {{ current_path }}
       <!--  -->
     </v-main>
   </v-app>
@@ -36,22 +42,68 @@ export default {
   data: () => ({
     drawer: null,
     hierarchy: null,
-    level1 : [],
-    // level2: null,
-    // level3: null,
-    // selected1: null
+    level1: [],
+    level2: [],
+    level3: [],
+    selected1: null,
+    selected2: null,
+    paths: {},
+    current_path: '',
+
   }),
   mounted() {
-    fetch('http://127.0.0.1:5000/get_hierarchy')
+    fetch('http://127.0.0.1:5000/l1')
       .then(response => response.json())
       .then(data => {
-        this.level1 = Object.keys(data)
-        this.hierarchy = data;
+        this.level1 = data
+        this.selected1 = this.level1[0]
+        this.update2()
+        this.update3()
+
+
       })
       .catch(error => {
         console.error('Error:', error);
       });
+
+
   },
+  methods: {
+    tap1(item) {
+      this.selected1 = item
+      this.update2()
+      this.update3()
+    },
+    tap2(item) {
+      this.selected2 = item
+      this.update3()
+    },
+    tap3(item) {
+      this.current_path = this.paths[item]
+    },
+    update2() {
+      fetch('http://127.0.0.1:5000/l2')
+        .then(response => response.json())
+        .then(data => {
+          this.level2 = data[this.selected1]
+          this.selected2 = this.level2[0]
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    },
+    update3() {
+      fetch('http://127.0.0.1:5000/l3')
+        .then(response => response.json())
+        .then(data => {
+          this.paths = data[this.selected1][this.selected2]['files']
+          this.level3 = Object.keys(this.paths)
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    }
+  }
 
 }
 </script>
